23e68044fb6481a401f639eb5534af8df94e53da
John O'Neill <johncloneill@gmail.com>
2018-08-19 18:41:14 -0700

    Fix sangrenel start script

--- terraform/remote_config_scripts/configure_and_run_sangrenel.sh
+++ terraform/remote_config_scripts/configure_and_run_sangrenel.sh
@@ -7,13 +7,14 @@ SANGRENEL_FLAG_MESSAGE_BATCH_SIZE=$4
 SANGRENEL_FLAG_NUM_WORKERS=$5
 
 KAFKA_BROKER_PORT=9092
-
 if [ $SHOULD_AUTO_START_TEST = "on" ]
 then
   KAFKA_BROKER_LIST=$(echo $KAFKA_BROKER_DNS_LIST | sed "s/,/:$KAFKA_BROKER_PORT,/g"):$KAFKA_BROKER_PORT
 
   # Run sangrenel!
   nohup ~/go/bin/sangrenel -topic stressed-out-topic -brokers $KAFKA_BROKER_LIST -message-size $SANGRENEL_FLAG_MESSAGE_SIZE -message-batch-size $SANGRENEL_FLAG_MESSAGE_BATCH_SIZE -workers $SANGRENEL_FLAG_NUM_WORKERS &
+  echo "Running sangrenel test"
+  sleep 1
 else
   echo "Not running sangrenel test"
 fi
